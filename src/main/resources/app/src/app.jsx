db45c026e50a52a6c205cce0493a81ce6fdbe92f
Zsolt <ghost990@gmail.com>
2017-07-27 22:15:31 +0200

    started react project (not working yet)

---
+++ src/main/resources/app/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/index.scss';
+
+import Header from './components/Header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
---
+++ src/main/resources/app/src/components/Header.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+            <div className="primary">
+                <div className="container">
+                    <div className="row header-row align-items-center">
+                        <div className="col col-lg-3 col-md-3 col-sm-8 col-xs-8 logo-wrapper">
+                            <div className="logo"></div>
+                        </div>
+                        <nav className="col col-7 text-right menu hidden-md-down">
+                            <ul className="main-menu list-unstyled">
+                                <li className="active">
+                                    <a href="#">
+                                        Messageboard
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Admin
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div className="col col-lg-2 hidden-md-down user-profile-wrapper">
+                            <div className="user-profile">
+                                <div className="image" data-toggle="tooltip" data-placement="bottom" title="Go to Your Profile">
+                                    <a href="#">
+                                        <img src="assets/public/profile/harry_smith.png" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+                        <div className="mobile-menu-open">
+                            <ul className="mobile-menu list-unstyled">
+                                <li>Message Board</li>
+                                <li>Admin</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="secondary">
+                <div className="container">
+                    <div className="row align-items-center secondary-row">
+                        <div className="col hidden-md-down">
+                            <div className="today-info">
+                                <date className="date">
+                                    Today is 2017. jun 21.
+                                </date>
+                                <div className="birthday">
+                                    Kanye West's birthday. <a href="#">Send him</a> your best wishes!
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="upcoming-events text-right">
+                                <div className="new-notification">
+                                    <a href="#">
+                                        <span className="text">
+                                        1 new notification
+                                    </span>
+                                        <span className="icon"></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+      </div>
+    );
+  }
+}
