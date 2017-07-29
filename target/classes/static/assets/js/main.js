8a07937af33bd824f637ba59bbc377c777e759a6
Zsolt <ghost990@gmail.com>
2017-07-29 18:31:31 +0200

    Deleted target, added new gitignore entry

--- target/classes/static/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$(".reply-action").click(function() {
-    $(this).closest( ".replies" ).slideToggle( "slow" );
-});
-
-$("#mobile-menu-button").click(function() {
-    $("#mobile-menu-open").slideToggle( "slow" );
-});
