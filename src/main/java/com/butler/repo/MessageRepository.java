fa537f0caecec379ea2b98b143eb66989159e546
Száraz Dénes <szarazdenes@gmail.com>
2017-08-29 11:34:11 +0200

    test again

--- src/main/java/com/butler/repo/MessageRepository.java
+++ src/main/java/com/butler/repo/MessageRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.butler.model.Message;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MessageRepository extends CrudRepository<Message, Long>{
-    List<Message> findByUser(long id);
+public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findAll();
 }
