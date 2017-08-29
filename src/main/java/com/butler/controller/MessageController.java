c192cbf879564ce4f9fc3789a266fc9beada9c76
Száraz Dénes <szarazdenes@gmail.com>
2017-08-29 11:29:15 +0200

    Tries

--- src/main/java/com/butler/controller/MessageController.java
+++ src/main/java/com/butler/controller/MessageController.java
@@ -1,19 +1,25 @@
 package com.butler.controller;
 
+import com.butler.model.Message;
 import com.butler.repo.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/message")
 public class MessageController {
 
-    @Autowired
-    MessageRepository messageRepository;
+    private final MessageRepository messageRepository;
 
-    @RequestMapping("/message/findall")
-    public String findAllMessage() {
-        return "LOL";
+    @Autowired
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @RequestMapping("/findall")
+    public Iterable<Message> getMessages() {
+        return  messageRepository.findAll();
     }
 
 }
