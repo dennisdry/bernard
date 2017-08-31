153aca2de40f3c55242acccb77f931bbaf6ec754
Száraz Dénes <szarazdenes@gmail.com>
2017-08-31 20:33:48 +0200

    date

--- src/main/java/com/bernardapp/model/TestMessage.java
+++ src/main/java/com/bernardapp/model/TestMessage.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class TestMessage {
@@ -13,12 +14,15 @@ public class TestMessage {
 
     private String message;
 
+    private Date date;
+
     public TestMessage() {
 
     }
 
-    public TestMessage(String message) {
+    public TestMessage(String message, Date date) {
         this.message = message;
+        this.date = date;
     }
 
     public long getId() {
@@ -27,6 +31,14 @@ public class TestMessage {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 
     public String getMessage() {
